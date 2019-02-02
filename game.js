afca2d5cd3a14e041ee27ea729ddef51f6cfe91a
Stepan <stepos36@gmail.com>
2019-02-02 10:15:42 -0500

    wordbank updated//bug fixes

--- game.js
+++ game.js
@@ -1,7 +1,61 @@
 var Word = require('./word.js');
 var inquirer = require('inquirer');
 var colors = require('colors');
-var wordBank = ['Hello', 'Goodbye'];
+var wordBank = [    "Alaska", 
+                    "Alabama", 
+                    "Arkansas", 
+                    "American Samoa", 
+                    "Arizona", 
+                    "California", 
+                    "Colorado", 
+                    "Connecticut", 
+                    "District of Columbia", 
+                    "Delaware", 
+                    "Florida", 
+                    "Georgia", 
+                    "Guam", 
+                    "Hawaii", 
+                    "Iowa", 
+                    "Idaho", 
+                    "Illinois", 
+                    "Indiana", 
+                    "Kansas", 
+                    "Kentucky", 
+                    "Louisiana", 
+                    "Massachusetts", 
+                    "Maryland", 
+                    "Maine", 
+                    "Michigan", 
+                    "Minnesota", 
+                    "Missouri", 
+                    "Mississippi", 
+                    "Montana", 
+                    "North Carolina", 
+                    "North Dakota", 
+                    "Nebraska", 
+                    "New Hampshire", 
+                    "New Jersey", 
+                    "New Mexico", 
+                    "Nevada", 
+                    "New York", 
+                    "Ohio", 
+                    "Oklahoma", 
+                    "Oregon", 
+                    "Pennsylvania", 
+                    "Puerto Rico", 
+                    "Rhode Island", 
+                    "South Carolina", 
+                    "South Dakota", 
+                    "Tennessee", 
+                    "Texas", 
+                    "Utah", 
+                    "Virginia", 
+                    "Virgin Islands", 
+                    "Vermont", 
+                    "Washington", 
+                    "Wisconsin", 
+                    "West Virginia", 
+                    "Wyoming"];
 var randNum;
 var unknownWord;
 var unknownWordLetters;
@@ -13,13 +67,14 @@ var pickWord = function(){
     startGame()
 }
 function startGame() {
-    var comparsionWord = unknownWordLetters.returnWord().replace(/ /g, '')
-    if((comparsionWord!=unknownWord.toLowerCase())&&(unknownWordLetters.attempts>0)) {
+    var comparisonWord = unknownWordLetters.returnWord().replace(/ /g, '')
+    if((comparisonWord!=unknownWord.toLowerCase())&&(unknownWordLetters.attempts>0)) {
         console.log('========================================='.rainbow+'\n'
         +unknownWordLetters.returnWord()+'\n'+'========================================='.rainbow+'\n'
         +'You have '+unknownWordLetters.attempts+' attempts left'+'\n');
         inquirer.prompt([
             {
+                type: 'input',
                 name: 'letter',
                 message: 'Type your guess'
             }
@@ -37,8 +92,23 @@ function endGame() {
         console.log('\n'+'Congratulations! You won!'.bgCyan+'\n');
     }
     else{
-        console.log('You lost! Correct answer was: '+unknownWord+'\n')
+        console.log('You lost! Correct answer was: '.bgRed+unknownWord+'\n')
     }
-    pickWord()
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'continue',
+            message: 'Do you want to play again?'
+        }
+    ]).then(function(response){
+        if(response.continue){
+            pickWord()
+        }
+        else{
+
+        }
+    })
+
+    
 }
 pickWord()

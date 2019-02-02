d5f3149dbb462c09a3d03ab5b5650afb0ff57db7
Stepan <stepos36@gmail.com>
2019-02-01 22:59:12 -0500

    letter.js

---
+++ letter.js
@@ -0,0 +1,18 @@
+var Letter = function(letter) {
+    this.char = letter;
+    this.guessed = false
+    this.response = function() {
+        if (this.guessed) {
+            return this.char;
+        }
+        else {
+            return '_';
+        }
+    }
+    this.checkLetter = function(){
+        if (userGuess.toLowerCase()===this.char){
+            this.guessed = true
+        }
+    }
+}
+module.exports Letter
